e72b077aa58a24dcb008c0870a4e562f4ded6cf5
map220v <map220v300@gmail.com>
2021-04-24 07:38:44 +0300

    build_kernel.sh now doesn't run configure if .config exists

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,5 +6,7 @@ export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 export SEC_BUILD_OPTION_SELINUX_ENFORCE=false
 
-make ARCH=arm64 exynos9610-a51xx_defconfig
+if [ ! -f .config ]; then
+	make ARCH=arm64 exynos9610-a51xx_defconfig
+fi
 make ARCH=arm64 -j4
