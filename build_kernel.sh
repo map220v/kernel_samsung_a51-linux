1fcf89a340a8604ee502c2c187cb38f863bc5fe3
map220v <map220v300@gmail.com>
2021-08-03 22:09:01 +0300

    updated config and build_kernel.sh script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
-export ARCH=arm64
-export SEC_BUILD_OPTION_SELINUX_ENFORCE=false
+#export PLATFORM_VERSION=11
+#export ANDROID_MAJOR_VERSION=r
+#export ARCH=arm64
+#export SEC_BUILD_OPTION_SELINUX_ENFORCE=false
 
 if [ ! -f .config ]; then
-	make ARCH=arm64 a51-linux11_defconfig
+	make a51-linux11_defconfig
 fi
-make ARCH=arm64 -j4
+make -j4
